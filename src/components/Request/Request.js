e7d360e6c628269bf384450b20bf9356f80ff16c
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-05-03 17:59:00 -0700

    added some css for the request component

--- src/components/Request/Request.js
+++ src/components/Request/Request.js
@@ -4,27 +4,23 @@ import styles from './Request.css';
 
 const Request = ({ url, onURLChange, body, onBodyChange, onMethodChange, onSubmit }) => (
   <form className={styles.Request} onSubmit={onSubmit}>
-    <input type="text" value={url} onChange={onURLChange} />
-    <label>
-        POST:
-      <input type="radio" name="method" value="POST" onChange={onMethodChange} />
-    </label>
-    <label>
-        GET:
-      <input type="radio" name="method" value="GET" onChange={onMethodChange} />
-    </label>
-    <label>
-        PATCH:
-      <input type="radio" name="method" value="PATCH" onChange={onMethodChange} />
-    </label>
-    <label>
-        PUT:
-      <input type="radio" name="method" value="PUT" onChange={onMethodChange} />
-    </label>
-    <label>
-        DELETE:
-      <input type="radio" name="method" value="DELETE" onChange={onMethodChange} />
-    </label>
+    <input type="text" value={url} onChange={onURLChange}/>
+    
+    <input id="POST" type="radio" name="method" value="POST" onChange={onMethodChange}/>
+    <label htmlFor="POST">POST</label>
+
+    <input id="GET" type="radio" name="method" value="GET" onChange={onMethodChange}/>
+    <label htmlFor="GET">GET</label>
+
+    <input id="PATCH" type="radio" name="method" value="PATCH" onChange={onMethodChange}/>
+    <label htmlFor="PATCH">PATCH</label>
+
+    <input id="PUT" type="radio" name="method" value="PUT" onChange={onMethodChange}/>
+    <label htmlFor="PUT">PUT</label>
+
+    <input id="DELETE" type="radio" name="method" value="DELETE" onChange={onMethodChange}/>
+    <label htmlFor="DELETE">DELETE</label>
+    
     <textarea value={body} onChange={onBodyChange}></textarea>
     <button>Go</button>
   </form>
