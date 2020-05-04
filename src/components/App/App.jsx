2f1b9c2e2ed69f1287bbd91d73b33869d93c8d98
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-05-03 18:52:43 -0700

    started fetch call

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Request from '../Request/Request.js';
+import Resty from '../../containers/Resty/Resty.jsx';
 
 export default function App() {
   return (
-    <Request />
+    <Resty />
   );
 }
   
---
+++ src/services/makeRequest.js
@@ -0,0 +1,9 @@
+// export const makeRequest = (url, method, body) => {
+//   return fetch(url, {
+//     method,
+//     body: check method ? body : null 
+//     // only if POST/PATCH?PUT with ternary
+//   })
+//   .then(res => res.json()
+//   .then(...)
+//   )};
