1c178a5eecc3c762b4765cb728af7f1895e930ef
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-05-08 14:14:56 -0700

    makeRequest

--- src/containers/Resty/Resty.jsx
+++ src/containers/Resty/Resty.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
+import { makeRequest } from '../../services/makeRequest.js';
 import Request from '../../components/Request/Request.js';
+// import Response from '../';
 
 const Resty = () => {
   const [url, setURL] = useState('');
   const [method, setMethod] = useState('POST');
   const [body, setBody] = useState('');
+  const [response, setResponse] = useState([]);
 
   const handleChange = ({ target }) => {
     if(target.value === 'url')setURL(target.value);
@@ -12,20 +15,23 @@ const Resty = () => {
     if(target.value === 'body')setBody(target.value);
   };
   
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = event => {
+    event.preventDefault();
     makeRequest(url, method, body)
       .then(json => setResponse(json));
   };
 
   return (
-    <Request 
-      url={url} 
-      method={method} 
-      body={body}
-      onChange={handleChange}
-      onSubmit={handleSubmit}
-    />
+    <>
+      <Request 
+        url={url} 
+        method={method} 
+        body={body}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+      <Response response={response} />
+    </>
   );
 
 };
